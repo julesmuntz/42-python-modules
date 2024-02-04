7fec5ef1a8fdf6455e7fde94134b2cd589d9debe
Jules MUNTZ-BERGER <julmuntz@student.42.fr>
2024-02-04 19:05:04 +0100

    py00 | ex09 done

---
+++ py00/ex09/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name="ft_package",
+    version="0.0.1",
+    description="A sample test package",
+    url="http://example.com",
+    author="Jules Muntz Berger",
+    author_email="julmuntz@student.42.fr",
+    license="MIT",
+)
+
+
+def main():
+    return
+
+
+if __name__ == "__main__":
+    main()
