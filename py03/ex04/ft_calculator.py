06e85bb99e88d10ffd8a39d357aa2cedf1e1bacb
Jules MUNTZ-BERGER <julmuntz@student.42.fr>
2024-02-13 15:07:57 +0100

    py03 | ex04 done

---
+++ py03/ex04/ft_calculator.py
@@ -0,0 +1,14 @@
+class calculator:
+    """This is a calculator class"""
+
+    def dotproduct(V1: list[float], V2: list[float]) -> None:
+        dot_product = sum(V1[i] * V2[i] for i in range(len(V1)))
+        print(f"Dot product is: {dot_product}")
+
+    def add_vec(V1: list[float], V2: list[float]) -> None:
+        add_vector = [V1[i] + V2[i] for i in range(len(V1))]
+        print(f"Add Vector is: {add_vector}")
+
+    def sous_vec(V1: list[float], V2: list[float]) -> None:
+        sous_vector = [V1[i] - V2[i] for i in range(len(V1))]
+        print(f"Sous Vector is: {sous_vector}")
